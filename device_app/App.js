cd17d771e74aff719eabe0eee7b4ac17661f5cd8
hoatruonghong <70649552+hoatruonghong@users.noreply.github.com>
2022-04-23 17:27:55 +0700

    init file

---
+++ device_app/App.js
@@ -0,0 +1,68 @@
+import { StyleSheet, Text, View, Button, Image } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import {Camera} from 'expo-camera';
+
+export default function App() {
+  const [hasCameraPermission, setHasCameraPermission] = useState(null);
+  const [camera, setCamera] = useState(null);
+  const [image, setImage] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.front);
+
+  useEffect(() => {
+    (async () => {
+      const cameraStatus = await Camera.requestCameraPermissionsAsync();
+      setHasCameraPermission(cameraStatus.status === 'granted');
+
+    })();
+  }, []);
+
+  const takePicture = async () => {
+    if (camera){
+      const data=await camera.takePictureAsync(null)
+      setImage(data.uri);
+    }
+  }
+
+  if (hasCameraPermission === null) {
+    return <View />;
+  }
+  if (hasCameraPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
+  return (
+    <View style={{flex:1}}>
+      <Home/>
+      <View style={styles.cameraContainer}>
+        <Camera ref={ref => setCamera(ref)}
+        style={styles.fixedRatio}
+        type={type}
+        ratio={'1:1'}   
+        />
+      </View>
+      <Button
+        title='Flip camera'
+        onPress={()=>{
+          setType(type === Camera.Constants.Type.back ? Camera.Constants.Type.front : Camera.Constants.Type.back);
+
+        }}></Button>
+      <Button
+        title='Take Picture'
+        onPress={()=>takePicture()}
+      />
+      {image && <Image source={{uri: image}} style={{flex:1}}/>}
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  cameraContainer: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+  fixedRatio:{
+    flex:1,
+    aspectRatio:1
+  }
+});
