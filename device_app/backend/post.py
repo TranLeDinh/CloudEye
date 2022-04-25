070dafeff1f5283cfb9129394494d8448fa41e58
TranDinhLe <75212466+TranDinhLe@users.noreply.github.com>
2022-04-25 08:31:26 +0700

    Update post.py

--- device_app/backend/post.py
+++ device_app/backend/post.py
@@ -36,44 +36,5 @@ client.on_subscribe = subscribe
 client.connect()
 client.loop_background()
 
-def getPort():
-    ports = serial.tools.list_ports.comports()
-    N = len(ports)
-    commPort = "None"
-    for i in range(0, N):
-        port = ports[i]
-        strPort = str(port)
-        if "USB Serial Device" in strPort:
-            splitPort = strPort.split(" ")
-            commPort = (splitPort[0])
-    return commPort
-
-isMicrobitConnected = False
-if getPort() != "None":
-    ser = serial.Serial( port=getPort(), baudrate=115200)
-    isMicrobitConnected = True
-
-
 def sendSignal():
   client.publish("bbc-temp", 1)
-
-"""mess = ""
-def readSerial():
-    bytesToRead = ser.inWaiting()
-    if (bytesToRead > 0):
-        global mess
-        mess = mess + ser.read(bytesToRead).decode("UTF-8")
-        while ("#" in mess) and ("!" in mess):
-            start = mess.find("!")
-            end = mess.find("#")
-            processData(mess[start:end + 1])
-            if (end == len(mess)):
-                mess = ""
-            else:
-                mess = mess[end+1:]
-
-
-while True:
-    if isMicrobitConnected:
-        readSerial()
-    time.sleep(1)"""
