629e4740490cb76749039e753a702a6160c6f3a7
phamnhatminh1292001 <66765903+phamnhatminh1292001@users.noreply.github.com>
2022-04-25 12:51:00 +0700

    Update test.py

--- device_app/backend/test.py
+++ device_app/backend/test.py
@@ -14,20 +14,15 @@ def test(train_path,test_path):
         TrainingImagePath=train_path
  
         from keras.preprocessing.image import ImageDataGenerator
-# Understand more about ImageDataGenerator at below link
-# https://blog.keras.io/building-powerful-image-classification-models-using-very-little-data.html
+
         classifier=load_model('keras_model.h5') 
-# Defining pre-processing transformations on raw images of training data
-# These hyper parameters helps to generate slightly twisted versions
-# of the original image, which leads to a better model, since it learns
-# on the good and bad mix of images
+
         train_datagen = ImageDataGenerator(
         shear_range=0.1,
         zoom_range=0.1,
         horizontal_flip=True)
  
-# Defining pre-processing transformations on raw images of testing data
-# No transformations are done on the testing images
+
         test_datagen = ImageDataGenerator()
  
 # Generating the Training Data
@@ -45,24 +40,20 @@ def test(train_path,test_path):
         batch_size=32,
         class_mode='categorical')
  
-# Printing class labels for each face
+
         test_set.class_indices
 
-# class_indices have the numeric tag for each face
+
         TrainClasses=training_set.class_indices
  
-# Storing the face and the numeric tag for future reference
+
 
         ResultMap={}
         for faceValue,faceName in zip(TrainClasses.values(),TrainClasses.keys()):
                 ResultMap[faceValue]=faceName
-# Saving the face map for future reference
- 
-# The model will give answer as a numeric tag
-# This mapping will help to get the corresponding face name for it
+
         print("Mapping of Face and its ID",ResultMap)
  
-# The number of neurons for the output layer is equal to the number of faces
         OutputNeurons=len(ResultMap)
         ImagePath=test_path
         test_image=image.load_img(ImagePath,target_size=(64, 64))
@@ -70,7 +61,7 @@ def test(train_path,test_path):
  
         test_image=np.expand_dims(test_image,axis=0)
         result=classifier.predict(test_image,verbose=0)
-#print(training_set.class_indices)
+
         print(100*result[0])
         p=100*max(result[0])
         if p>96.5:
