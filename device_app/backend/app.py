fa96417aef369cdebbbd48996a5fa2d1c5e68a21
hoatruonghong <70649552+hoatruonghong@users.noreply.github.com>
2022-04-27 02:16:02 +0700

    Update app.py

--- device_app/backend/app.py
+++ device_app/backend/app.py
@@ -1,128 +1,126 @@
 from dataclasses import field
 from datetime import datetime
 from email.quoprimime import body_check
+import imp
+from operator import iadd
+from tkinter import Variable
 from turtle import title
 from unittest import result
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, redirect, url_for, flash
+
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+from sqlalchemy import null
+
+from werkzeug.utils import secure_filename
 
 from test import test as tmtest
 from train import fulltrain as tmtrain
-from post import sendOpenSignal as Open
-from post import sendNotOpenSignal as NotOpen
+
+from post import post
+
+import os.path
+import os
+import json
 
 app = Flask(__name__)
 
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:''@localhost/cloudeye'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db = SQLAlchemy(app)
-ma = Marshmallow(app)
-
-class info(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title=db.Column(db.String(100))
-    body=db.Column(db.Text())
-    date = db.Column(db.DateTime, default = datetime.now)
-
-    def __init__(self, title, body):
-        self.title = title
-        self.body = body
+app.config['UPLOAD_RECFOLDER'] = 'D:/dataset/recognize'
+app.config['UPLOAD_ADDFOLDER'] = 'D:/dataset/add/'
 
 
-class infoSchema(ma.Schema):
-    class Meta:
-        fields = ('id', 'title', 'body', 'date')
+addid = 0
+recid = 0
 
-info_schema = infoSchema()
-infos_schema = infoSchema(many=True)
-
-@app.route('/', methods = ['GET'])
-def get_default():
-    #return jsonify({"hello": "world"})
-    all_infos = info.query.all()
-    results = infos_schema.dump(all_infos)
-    return jsonify(results)
-
-@app.route('/get', methods = ['GET'])
-def get_info():
-    #return jsonify({"hello": "world"})
-    all_infos = info.query.all()
-    results = infos_schema.dump(all_infos)
-    return jsonify(results)
-
-@app.route('/get/<id>/', methods = ['GET'])
-def get_detail_info(id):
-    #return jsonify({"hello": "world"})
-    info_detail = info.query.get(id)
-    return info_schema.jsonify(info_detail)
-
-
-@app.route('/update/<id>/', methods = ['PUT'])
-def update_info(id):
-    #return jsonify({"hello": "world"})
-    info_detail = info.query.get(id)
-    
-    title = request.json['title']
-    body = request.json['body']
-    
-    info_detail.title = title
-    info_detail.body = body
-    
-    db.session.commit()
-    return info_schema.jsonify(info_detail)
-
-
-@app.route('/delete/<id>/', methods = ['DELETE'])
-def delete_info(id):
-    #return jsonify({"hello": "world"})
-    info_detail = info.query.get(id)
-    
-    db.session.delete(info_detail)
-    db.session.commit()
-    
-    return info_schema.jsonify(info_detail)
-
-
-@app.route('/add', methods = ['POST'])
-def add_info():
-    title = request.json['title']
-    body = request.json['body']
-    
-    infos = info(title, body)
-    db.session.add(infos)
-    db.session.commit()
-    return info_schema.jsonify(infos)
-
-@app.route('/open')
-def open():
-    sendOpenSignal()
-
-@app.route('/notopen')
-def not_open():
-    sendNotOpenSignal()
-
-@app.route('/taketrainpath', methods=['POST'])
-def take_train_path():
-    global train_path
-    request_data = json.loads(request.data)
-    train_path = request_data['content']
-    
-@app.route('/taketestpath', methods=['POST'])
-def take_test_path():
-    global image_path
-    request_data = json.loads(request.data)
-    image_path = request_data['content']
 
 @app.route('/test')
-def test():
-  return tmtest(train_path,image_path)
+def test():    
+    train_path=app.config['UPLOAD_RECFOLDER']
+    image_path=folder
+    
+    re = tmtest(train_path,image_path)
+    
+    print(re['return'])
+    if re['return'] != "Nguoi la":
+        post(2)
+    else:
+        post(3)
+    return jsonify(re)
+
 
 @app.route('/train')
 def train():
-  tmtrain(train_path)
-    
-if __name__ == "main":
-    app.run(debug=True)
+    train_path=app.config['UPLOAD_ADDFOLDER']
+    tmtrain(train_path)
+    return ''
+
+@app.route('/recognize', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            #flash('No file part')
+            return redirect(request.url)
+        
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            #flash('No selected file')
+            return redirect(request.url)
+        if file:
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(folder, filename))
+                        
+            return redirect(url_for('upload_file', name=filename))
+    return ''
+
+@app.route('/add', methods=['GET', 'POST'])
+def upload_addfile():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            #flash('No file part')
+            return redirect(request.url)
+        
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            #flash('No selected file')
+            return redirect(request.url)
+        if file:
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(folder, filename))
+                        
+            return redirect(url_for('upload_addfile', name=filename))
+    return ''
+
+
+@app.route('/addfolder')
+def add_addfolder():
+    global addid
+    foldername = 'add'+ str(addid)+'/'
+    addid += 1
+    global folder
+    folder = os.path.join(app.config['UPLOAD_ADDFOLDER'], foldername)
+    if not os.path.exists(folder):        
+        os.makedirs(folder)
+    return ''
+
+@app.route('/recfolder')
+def add_recfolder():    
+    global recid
+    foldername = 'rec'+ str(recid)+'/'
+    recid += 1
+    global folder
+    folder = os.path.join(app.config['UPLOAD_RECFOLDER'], foldername)
+    if not os.path.exists(folder):        
+        os.makedirs(folder)
+    return ''
+
+if __name__ == "__main__":
+    app.run(host = '192.168.42.127', port=8081, debug=True)
